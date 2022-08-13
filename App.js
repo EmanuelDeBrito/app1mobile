e429f52863d430ce54273cdbe26582a2078e79f3
EmanuelDeBrito <emanuelbrit16@gmail.com>
2022-08-13 01:39:14 +0000

    Criando um layout  básico

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
+<View  style={styles.containerprinc}>
     <View style={styles.container}>
-      <Text>aula de mobile e muito bom</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textotitulo}>Aula de Mobile</Text>
+		  <View style={styles.bloco}>
+			  <Text style={styles.titulo}>Calculadora Simples</Text>
+		  </View>
+		  <View style={styles.bloco}>
+			  <Text>Valor 1:</Text>
+			  <TextInput style={styles.input}/>
+		  </View>
+		  <View style={styles.bloco}>
+			  <Text>Valor 2:</Text>
+			<TextInput style={styles.input}/>		  
+		  </View>
+		  <View style={styles.bloco2}>
+		  	<TouchableOpacity style={styles.botao}>
+				<Text style={styles.textoBotao}>Somar</Text>
+			</TouchableOpacity>
+		  </View>
     </View>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+	 
+    
   },
+	containerprinc:{
+		marginTop:'30%'
+	},
+	bloco:{
+		marginTop:20,
+	    width:'80%',
+		marginLeft:'10%'
+	},
+	bloco2:{
+		marginTop:20,
+	    width:'80%',
+		marginLeft:'10%',
+		alignItems:'center'
+	},
+	titulo:{
+		fontSize:30,
+		textAlign:'center'
+	},
+	input:{
+		borderWidth:2,
+		fontSize:20,
+		borderRadius:20
+	},
+	botao:{
+		backgroundColor:'#000',
+		borderRadius:20,
+		width:'28%'
+		
+	},
+	textoBotao:{
+		color:'#FFF',
+		textAlign:'center',
+		fontsize:20
+	},
+	textotitulo:{
+		textAlign:'center',
+		fontSize: 20,
+		
+}
 });
